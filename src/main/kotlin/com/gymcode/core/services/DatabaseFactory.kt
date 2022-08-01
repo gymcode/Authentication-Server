2ff6c43fe436b8d8827fb85e8b348c357ce56b24
gymcode <kyleabs20@gmail.com>
2022-08-01 22:07:24 +0000

    database configuration

--- src/main/kotlin/com/gymcode/core/services/DatabaseFactory.kt
+++ src/main/kotlin/com/gymcode/core/services/DatabaseFactory.kt
@@ -1,8 +1,32 @@
 package com.gymcode.core.services
 
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
+
 object DatabaseFactory {
 
     fun init(){
-
+        Database.connect(hikari())
+        databaseTransaction()
     }
+
+    private fun hikari(): HikariDataSource{
+        val config = HikariConfig()
+        config.driverClassName = "com.mysql.jdbc.Driver"
+        config.username = "root"
+        config.password = "H@ppy123"
+        config.jdbcUrl = "jdbc:mysql://localhost:3306/pakro-authentication"
+        return HikariDataSource(config)
+    }
+
+    private fun databaseTransaction(){
+        transaction {
+            // database table creation goes here
+
+        }
+    }
+
 }
