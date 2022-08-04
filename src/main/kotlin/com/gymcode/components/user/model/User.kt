530c8c99c351ef186b5aa0351a9510101a82a8db
gymcode <kyleabs20@gmail.com>
2022-08-04 23:08:37 +0000

    user model and dao

--- src/main/kotlin/com/gymcode/components/user/model/User.kt
+++ src/main/kotlin/com/gymcode/components/user/model/User.kt
@@ -1,11 +1,14 @@
 package com.gymcode.components.user.model
 
 import com.gymcode.core.shared.MODEL_VERSION
-import kotlinx.serialization.Contextual
-import kotlinx.serialization.Required
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.Transient
+import kotlinx.serialization.*
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import java.util.UUID
 
 @Serializable
@@ -18,19 +21,51 @@ data class User(
     val lastName: String,
     val otherNames: String? = "",
     @Transient
-    val password: String? = null,
-    val houseAddress: String? = null,
+    val password: String? = "",
+    val houseAddress: String? = "",
     @Required
     val email: String,
     @Required
     val postalAddress: String,
-    @Serializable(with = )
+    @Serializable(with = LocalDateTimeSerializer::class)
     val createdDate: LocalDateTime = LocalDateTime.now(),
+    @Serializable(with = LocalDateTimeSerializer::class)
     val updatedDate: LocalDateTime = LocalDateTime.now(),
     val version: Long = MODEL_VERSION
 )
 
 @Serializable
 data class UserDTO(
+    @Required
+    val firstName: String,
+    @Required
+    val lastName: String,
+    val otherNames: String?,
+    @Required
+    val password: String,
+    @Required
+    val houseAddress: String,
+)
+
+@Serializable
+data class SignUpDTO(
+    @Required
     val
 )
+
+class LocalDateTimeSerializer : KSerializer<LocalDateTime> {
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("LocalDateTime", PrimitiveKind.STRING)
+
+    /**
+     * The ISO-like date-time formatter that formats or parses a date-time with the offset and zone if available,
+     * such as '2011-12-03T10:15:30', '2011-12-03T10:15:30+01:00' or '2011-12-03T10:15:30+01:00[Europe/Paris]'.
+     * */
+    override fun serialize(encoder: Encoder, value: LocalDateTime) {
+        encoder.encodeString(value.format(DateTimeFormatter.ISO_DATE_TIME).toString())
+    }
+
+    override fun deserialize(decoder: Decoder): LocalDateTime {
+        val string = decoder.decodeString()
+        return LocalDateTime.parse(string)
+    }
+}
--- src/main/kotlin/com/gymcode/core/shared/staticVars.kt
+++ src/main/kotlin/com/gymcode/core/shared/staticVars.kt
@@ -1,3 +1,4 @@
 package com.gymcode.core.shared
 
 const val MODEL_VERSION = 1L
+const val BASE_URL = "api/v1/client"
